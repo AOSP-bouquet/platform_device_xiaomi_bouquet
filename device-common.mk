b4067858c55de2abe04cfc8ac6bbf60f4e1d743a
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    bouquet: Add basic USB HAL that reports no status change
    Signed-off-by: Pranaya Deomani <deomanipranaya007@gmail.com>
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -397,7 +397,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.bouquet
 
 # Vibrator
 PRODUCT_PACKAGES += \
