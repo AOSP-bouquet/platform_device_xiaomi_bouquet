47e37ba8fb01a890d35e7844fd4c06c9b2a1b7dc
Randall Huang <huangrandall@google.com>
2018-11-20 11:34:30 +0800

    Add health.storage@1.0 HAL implemenetation
    Test: lshal debug android.hardware.health.storage@1.0::IStorage/default
    Test: run `adb shell sm idle-maint run`
    
    Bug: 116499468
    
    Change-Id: I1e119e0fda155fd645ae2c88999c39394c478c6d
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- device-common.mk
+++ device-common.mk
@@ -230,6 +230,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service.bouquet
 
+# Storage health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health.storage@1.0-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
