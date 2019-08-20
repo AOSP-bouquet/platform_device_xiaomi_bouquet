ea1454479ae9921228b4eb5cc4b53baba6026671
Bruno Martins <bgcngm@gmail.com>
2019-08-20 21:47:56 +0100

    bouquet: rootdir: Create power init script
    Gather all performance related items in one place
    and drop all deprecated services.
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -248,7 +248,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qti.dcvs.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
