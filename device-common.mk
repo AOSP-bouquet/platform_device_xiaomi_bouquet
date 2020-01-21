7774799e4e785a1905438c4d65f379d35beb1e24
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    bouquet: Add powerhint.json for sdm636
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -320,7 +320,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_bouquet-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
