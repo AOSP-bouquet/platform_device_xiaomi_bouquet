083027ae807327949bb9e1dd81ab3869a8e9f1fc
Jason Chiu <jasoncschiu@google.com>
2020-03-10 20:58:58 +0800

    bouquet: Disable Rescue Party on userdebug & eng build
    Bug: 151151366
    Change-Id: I9823e1b194f1b7fc86c6a26943d6256f804efc6e
    Signed-off-by: Jason Chiu <jasoncschiu@google.com>
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- properties.mk
+++ properties.mk
@@ -235,3 +235,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.pmd=true \
     ro.vendor.sensors.sta_detect=true \
     ro.vendor.sensors.mot_detect=true
+
+# Disable Rescue Party on userdebug & eng build
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+endif
