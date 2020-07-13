5f8efdb88ea39987f0f42b4999fe3f268bcb6cf7
Michael Bestas <mkbestas@lineageos.org>
2020-07-13 19:40:01 +0300

    whyred: Remove ifaa/soter/mlipay support
    Change-Id: I93d0e3848d1d0119257c3ab2e8e8d11a6b753f00
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,6 @@ VENDOR_SECURITY_PATCH := 2018-11-01
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
