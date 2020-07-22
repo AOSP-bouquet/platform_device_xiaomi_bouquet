e25fe2559b326a1fba59a63164394291205e7c98
Bharat Pawar <bpawar@codeaurora.org>
2020-07-22 20:54:03 +0530

    sdm660-common: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.

--- sdm660.mk
+++ sdm660.mk
@@ -390,11 +390,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.whyred
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
