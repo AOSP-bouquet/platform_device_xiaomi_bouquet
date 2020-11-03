9f0aa245109e0c1c5d4fd3a153f03df86aef976f
Danny Lin <danny@kdrag0n.dev>
2020-11-03 11:27:23 -0800

    bouquet: Remove AOSP prefix
    This is how Google builds the Pixel stock ROM, and it makes more sense
    in my opinion: this is generally an Android device tree. Instead of
    branding the device differently in every custom ROM despite there being
    few actual differences, remove.
    
    Note that this has been added as a new product that overlays the AOSP
    product to avoid merge conflicts in AOSP updates from Google and ease
    commit exchanging with other ROMs based on AOSP trees.
    
    Change-Id: Ie3c401fd6e32dc7758b5590f9968ff3787687ebe
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- bouquet.mk
+++ bouquet.mk
@@ -6,6 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 #
 # All components inherited here go to system image
@@ -20,21 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 #
+# All components inherited here go to product image
+#
+$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
+
+#
 # All components inherited here go to vendor image
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 
-# Inherit some common HentaiOS stuff
-$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
-
-# Inherit from tulip device
-$(call inherit-product, $(LOCAL_PATH)/device-tulip.mk)
-
 PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := tulip
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := hentai_tulip
-PRODUCT_MODEL := Redmi Note 6 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
