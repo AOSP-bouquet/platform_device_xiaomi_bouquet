065cffa1ec5023a8ed79cffc16353b209938b5e0
Jason Wojcik <jwojcik@codeaurora.org>
2021-06-23 13:08:15 -0700

    bouquet: json-c: Remove LOCAL_COPY_HEADERS
    Remove deprecated LOCAL_COPY_HEADERS.
    Headers are exported via LOCAL_EXPORT_C_INCLUDE_DIRS.
    
    Change-Id: I78907fd28c4a9872b92bd1baa630c640a1162c5b

--- json-c/Android.mk
+++ json-c/Android.mk
@@ -8,20 +8,6 @@ LOCAL_NOTICE_FILE := $(LOCAL_PATH)/COPYING
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-LOCAL_COPY_HEADERS_TO := libjson/inc
-LOCAL_COPY_HEADERS := bits.h \
-		config.h \
-		debug.h \
-		linkhash.h \
-		arraylist.h \
-		json.h \
-		json_config.h \
-		json_inttypes.h \
-		json_util.h \
-		json_object.h \
-		json_tokener.h \
-		json_object_iterator.h \
-		json_c_version.h
 LOCAL_SRC_FILES := arraylist.c \
 		debug.c \
 		json_c_version.c \
