078d2a316fc7eba2c08b5b010ec5242127763ec8
yukiscape <garikapatiamartya@gmail.com>
2021-08-08 15:57:25 +0600

    bouquet: Nuke Camera2
    * Built by default and is pretty useless since most use Google Camera
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Drive NgaResources GoogleTTS Music PrebuiltGmail RecorderPrebuilt YouTube YouTubeMusicPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES := Camera2 Chrome Chrome-Stub Maps Drive NgaResources GoogleTTS Music PrebuiltGmail RecorderPrebuilt YouTube YouTubeMusicPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
