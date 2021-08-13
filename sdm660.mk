3666e170d83c3650e4e8983fd7869b450b49d67b
Reinazhard <reinazhard@gmail.com>
2021-08-13 19:38:59 +0700

    sdm660-common: remove lineage leftover
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -283,11 +283,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm660
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -389,10 +384,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
-
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # Usb
 PRODUCT_PACKAGES += \
