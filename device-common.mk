089a6636470ecf70d6505e8016bf212f81824191
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    bouquet: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device-common.mk
+++ device-common.mk
@@ -386,6 +386,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_shim
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
