42ee9e55b60307aea5688a5ca44448bebfcd6d6b
StyloGey <stylogey@gmail.com>
2021-09-20 00:51:13 +0200

    bouquet: use sf phase offsets from bonito
    * Less jank was observed while opening apps
    
    Change-Id: I06f0ed843bf7a460831bd06a8ac3bfbbf2ad5694
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- properties.mk
+++ properties.mk
@@ -133,18 +133,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.use_phase_offsets_as_durations=1 \
-    debug.sf.late.sf.duration=10500000 \
-    debug.sf.late.app.duration=16600000 \
-    debug.sf.early.sf.duration=16600000 \
-    debug.sf.early.app.duration=16600000 \
-    debug.sf.earlyGl.sf.duration=16600000 \
-    debug.sf.earlyGl.app.duration=16600000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_phase_offset_ns=1500000 \
     ro.opengles.version=196610 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
