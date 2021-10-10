f007a6506851ae5f7c8d199cb5385c4f02cdc4e6
Reinazhard <reinazhard@gmail.com>
2021-10-10 13:36:10 +0000

    bouquet: thermal: rename to Xiaomi bouquet
    to avoid conflict with hardware/qcom/thermal
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -411,7 +411,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.xiaomi_bouquet.qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
