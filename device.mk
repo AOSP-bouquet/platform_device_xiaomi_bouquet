44e8b886f602d6d542e67aa9599181e1a00b8558
clarencelol <clarencekuiek@icloud.com>
2021-10-15 14:39:54 +0800

    whyred: Build libldacBT_bco from source
    * E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found

--- device.mk
+++ device.mk
@@ -133,6 +133,7 @@ PRODUCT_BOOT_JARS += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbthost_if \
+    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
