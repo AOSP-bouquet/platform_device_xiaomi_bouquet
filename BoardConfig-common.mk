970cde966d4033a02081da612f12b243dbca8cc2
DillerOFire <niktofe1@gmail.com>
2021-10-24 18:10:16 +1000

    bouquet: Enable dark bootanimation
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -94,7 +94,8 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.configfs=true \
     androidboot.usbcontroller=a800000.dwc3 \
     loop.max_part=7 \
-    kpti=off
+    kpti=off \
+    androidboot.theme=1
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
