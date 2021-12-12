22cefe41f849c9a847da06e29124199ec2cc5970
Reinazhard <reinazhard@gmail.com>
2021-12-12 19:04:16 +0100

    bouquet: Spoof device to redfin
    also simplified the prop
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- hentai_whyred.mk
+++ hentai_whyred.mk
@@ -45,6 +45,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.037 7862242 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.211205.008 7888514 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SD1A.210817.037/7862242:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys
