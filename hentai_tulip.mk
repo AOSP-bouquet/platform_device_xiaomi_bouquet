d7d6be0e5df2a3ac6c978fa38d596786cb342085
Reinazhard <reinazhard@gmail.com>
2022-01-06 19:42:23 +0100

    bouquet: Update redfin fp
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- hentai_tulip.mk
+++ hentai_tulip.mk
@@ -42,6 +42,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.211205.008 7888514 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
