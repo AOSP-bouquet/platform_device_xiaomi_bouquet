75dd7268a9076c866d6a75d9370087fed987fa7a
Reinazhard <reinazhard@gmail.com>
2022-02-05 03:00:36 +0100

    bouquet: Refactor tree
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-whyred.mk
+++ device-whyred.mk
@@ -10,13 +10,6 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 # Inherit from bouquet-common
 $(call inherit-product, device/xiaomi/bouquet/device-common.mk)
 
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/whyred/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(LOCAL_PATH)/whyred/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/whyred/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/whyred/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
@@ -33,10 +26,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FrameworkResOverlayWhyred \
     SystemUIGoogleOverlayWhyred
-
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/whyred/init.whyred.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.device.rc
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
