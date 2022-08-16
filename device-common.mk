6615ccae80b7430a0f1bf27df99501afb492a4c6
electimon <electimon@gmail.com>
2022-08-16 09:43:13 +0800

    bouquet: Remove audio.a2dp.default from PRODUCT_PACKAGES
    * This module is not to be built this way anymore
    
    Signed-off-by: electimon <electimon@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -84,7 +84,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \
