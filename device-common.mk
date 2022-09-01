3181685066fe1657be3ef73809f5dfa74fc37dba
Reinazhard <reinazhard@gmail.com>
2022-09-01 17:46:13 +0000

    lights
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -299,7 +299,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.bouquet
+    android.hardware.light-service.bouquet
 
 # Media
 PRODUCT_COPY_FILES += \
