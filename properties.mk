db842078dbf9fa14487f26af965e36fc1b4175d1
Reinazhard <reinazhard@gmail.com>
2022-10-31 07:35:31 +0000

    bouquet: Disable ZRAM writeback
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- properties.mk
+++ properties.mk
@@ -243,9 +243,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Subsystem silent restart
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.ssr.restart_level=ALL_ENABLE
-
-# ZRAM writeback
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=1440 \
-    ro.zram.periodic_wb_delay_hours=24
