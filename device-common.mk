3ed31c525b6a1c775c0043c2f38a1b691a629f6a
Reinazhard <reinazhard@gmail.com>
2023-02-19 08:46:46 +0100

    bouquet: Use power-libperfmgr from hardware/xiaomi
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -364,7 +364,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_bouquet-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
