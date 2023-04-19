e265a20a486824c3e3f3801334f72592c8c44443
Reinazhard <reinazhard@gmail.com>
2023-04-19 07:38:05 +0200

    Revert "bouquet: Drop non-functional soundtrigger"
    This reverts commit 890e3e0169c1696f7f6f5d08b8e2faccfe551bb6.

--- device-common.mk
+++ device-common.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.3-impl:32 \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \
